3eb020c12d68db6b592696ee383cd685d8181bac
Liudas Baronas <liudasbar2@gmail.com>
2021-03-11 13:12:14 +0200

    Mail compose VC polishing

--- iOSTask/Details/View/SendMail.swift
+++ iOSTask/Details/View/SendMail.swift
@@ -8,40 +8,32 @@
 import Foundation
 import MessageUI
 
-extension DetailsVC {
+class Mail {
+    
+    var delegate: DetailsVC?
     
     /// Send mail execution function
-    func sendMail(_ type: String) {
-        let deviceName = UIDevice.current.localizedModel
-        let systemName = UIDevice.current.systemName
-        let systemVersion = UIDevice.current.systemVersion
-        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-        let appBuildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
-        let screenWidth = Int(UIScreen.main.nativeBounds.width)
-        let screenHeight = Int(UIScreen.main.nativeBounds.height)
-        
+    func sendMail(_ email: String) {
         if MFMailComposeViewController.canSendMail() {
             let mail = MFMailComposeViewController()
-            mail.mailComposeDelegate = self
-            mail.setToRecipients(["me@liudasbar.dev"])
-            mail.setSubject("\(type): Network Lantern")
-            mail.setMessageBody("\n\n\nPlease add relevant screenshots via attachments.\n\nDevice: \(deviceName)\nSystem: \(systemName), \(systemVersion)\nApp Version: \(appVersion!)\nBuild Number: \(appBuildNumber!)\nScreen Size: \(screenWidth)x\(screenHeight)", isHTML: false)
+            mail.mailComposeDelegate = delegate
+            mail.setToRecipients([email])
+            mail.setSubject("")
+            mail.setMessageBody("", isHTML: false)
             
-            self.present(mail, animated: true)
+            delegate?.present(mail, animated: true)
             
         } else {
-            let errorAlert = UIAlertController(title: "Email can not be sent", message: "Mail application is probably not set up.", preferredStyle: .alert)
-            self.present(errorAlert, animated: true, completion: nil)
+            let alert = UIAlertController(title: "Email can not be sent", message: "Mail application is probably not set up.", preferredStyle: .alert)
+            delegate?.present(alert, animated: true, completion: nil)
             
-            errorAlert.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: { action in
-                return
-            }))
+            alert.addAction(UIAlertAction(title: "Retry", style: UIAlertAction.Style.default, handler: { action in }))
         }
     }
     
     /// Mail and all other related child view controllers dismissal when finished
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        presentingViewController?.dismiss(animated: true, completion: nil)
+        //delegate?.dismiss(animated: true, completion: nil)
         controller.dismiss(animated: true)
     }
 }
