0b3b8a86ace169fbf97bd437e601b77b34b9acae
Liudas Baronas <liudasbar2@gmail.com>
2021-03-11 14:42:11 +0200

    Details cells additions

---
+++ iOSTask/Details/TableView/TableViewDataCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewDataCells.swift
+//  iOSTask
+//
+//  Created by LiudasBar on 2021-03-11.
+//
+
+import Foundation
+import UIKit
+
+class DetailsDataCell: UITableViewCell {
+    
+    @IBOutlet weak var detailsIconImageView: UIImageView!
+    
+    @IBOutlet weak var detailsInfoLabel: UILabel!
+}
---
+++ iOSTask/Details/TableView/TableViewFirstCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewFirstCell.swift
+//  iOSTask
+//
+//  Created by LiudasBar on 2021-03-11.
+//
+
+import Foundation
+import UIKit
+
+class DetailsFirstCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
